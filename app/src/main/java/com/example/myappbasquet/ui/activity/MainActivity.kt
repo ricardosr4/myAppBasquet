d41d8800e62172d7e127d1f5c0a013babaa4938b
ricardosr4 <you@example.com>
2023-05-26 11:23:16 -0400

    creacion de navigation component y estructura de carpetas

--- app/src/main/java/com/example/myappbasquet/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myappbasquet/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myappbasquet
+package com.example.myappbasquet.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myappbasquet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
