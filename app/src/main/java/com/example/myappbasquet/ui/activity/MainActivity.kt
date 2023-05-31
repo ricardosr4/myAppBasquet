175e2228091129efb16ac4576247a597780fbb50
ricardosr4 <you@example.com>
2023-05-31 10:05:08 -0400

    splash

--- app/src/main/java/com/example/myappbasquet/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myappbasquet/ui/activity/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myappbasquet.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 import com.example.myappbasquet.R
+import com.example.myappbasquet.ui.fragment.PantallaUnoFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splashScreen.setKeepOnScreenCondition{false}
+        intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
 }
