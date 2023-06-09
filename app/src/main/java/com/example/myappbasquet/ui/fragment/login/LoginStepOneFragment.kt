8ce4c5edb177a8fa3e78b5630a2813cffbcbb34d
ricardosr4 <you@example.com>
2023-06-09 07:44:50 -0400

    login for google

--- app/src/main/java/com/example/myappbasquet/ui/fragment/login/LoginStepOneFragment.kt
+++ app/src/main/java/com/example/myappbasquet/ui/fragment/login/LoginStepOneFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myappbasquet.ui.fragment.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,45 +10,81 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myappbasquet.R
 import com.example.myappbasquet.databinding.FragmentLoginStepOneBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class LoginStepOneFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    lateinit var binding: FragmentLoginStepOneBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {}
-    }
+    private lateinit var binding: FragmentLoginStepOneBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentLoginStepOneBinding.inflate(inflater, container, false)
-        return  binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttongoogle.setOnClickListener {
-            findNavController().navigate(R.id.action_loginStepOneFragment_to_fragmentHome)
-        }
-        binding.buttonemail.setOnClickListener {
-            findNavController().navigate(R.id.action_loginStepOneFragment_to_loginRegisterFragment)
-        }
-        binding.txtbuttonlogin.setOnClickListener {
-           findNavController().navigate(R.id.action_loginStepOneFragment_to_loginStartFragment2)
-        }
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+        binding.buttongoogle.setOnClickListener {
+            signInWithGoogle()
+        }
     }
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginStepOneFragment().apply {
-                arguments = Bundle().apply {}
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 123)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account?.idToken)
+            } catch (e: ApiException) {
+                Toast.makeText(
+                    requireContext(),
+                    "Error al iniciar sesión con Google",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // El inicio de sesión con Google fue exitoso
+                    Toast.makeText(
+                        requireContext(),
+                        "Inicio de sesión con Google exitoso",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error al iniciar sesión con Google",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
     }
 }
