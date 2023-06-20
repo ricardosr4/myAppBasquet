6e4959766f80451ad355699b134266a362ed0ca3
ricardosr4 <you@example.com>
2023-06-20 08:15:16 -0400

    creacion de correo por cuenta

--- app/src/main/java/com/example/myappbasquet/ui/fragment/login/LoginStartFragment.kt
+++ app/src/main/java/com/example/myappbasquet/ui/fragment/login/LoginStartFragment.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.util.PatternsCompat
 import androidx.navigation.fragment.findNavController
 import com.example.myappbasquet.R
 import com.example.myappbasquet.databinding.FragmentLoginStartBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 
 class LoginStartFragment : Fragment() {
     // TODO: Rename and change types of parameters
     lateinit var binding: FragmentLoginStartBinding
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
+        auth = FirebaseAuth.getInstance()
 
-        }
     }
 
     override fun onCreateView(
@@ -32,6 +35,7 @@ class LoginStartFragment : Fragment() {
 
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ivback.setOnClickListener {
@@ -41,9 +45,41 @@ class LoginStartFragment : Fragment() {
             findNavController().navigate(R.id.action_loginStartFragment2_to_forgotPasswordFragment)
         }
         binding.irRegistrar.setOnClickListener {
-            findNavController().navigate(R.id.ir_registrar)
+            findNavController().navigate(R.id.action_loginStartFragment2_to_loginRegisterFragment2)
+        }
+
+        binding.btnAcceso.setOnClickListener {
+            findNavController().navigate(R.id.action_loginStartFragment2_to_fragmentHome)
+
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                registerUser(email, password)
+            } else {
+                Toast.makeText(context, "por favor complete todos los campos", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "registro exitoso", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(
+                        context,
+                        "error al registrar: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+    }
+
 
     companion object {
 
